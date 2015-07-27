d660791642847aa04caabcf6837b78e6b20d396c
Regan S Poston <mr.rsposton@gmail.com>
2015-07-26 21:24:37 -0700

    auth capabilities for the wine app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "sign_up" => "users#new", :as => "sign_up"
+##  root to: 'visitors#index'
+  root to: 'users#new'
+  resources :users
+  resources :sessions
 end
